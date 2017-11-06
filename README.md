50af1943cfda7698659144d200edac55df6aac2c
Jacques tatiana <tat.jacques@gmail.com>
2017-11-06 16:24:11 +0100

    Add new 3D object

--- README.md
+++ README.md
@@ -3,3 +3,8 @@ Sound-Visualizer
 
 A sound visualizer in Web Audio API and WebGL (Three.js)
 See demo at: http://srchea.com/apps/sound-visualizer-web-audio-api-webgl/
+`
+
+Run serveur
+ 
+`python -m SimpleHTTPServer 8000`
--- test_son/son génération 2/js/visualizer.js
+++ test_son/son génération 2/js/visualizer.js
@@ -14,8 +14,8 @@ camera.position.z = 50;
 document.body.appendChild(renderer.domElement);
 
 var createSphere = function() {
-    var geometry = new THREE.BoxGeometry( 1, 1, 1 );
-    var material = new THREE.MeshBasicMaterial( {color: 0x00ffff} );
+    var geometry = new THREE.SphereGeometry( 1, 50, 50 );
+    var material = new THREE.MeshBasicMaterial( {color: 0xffff00} );
 
     var sphere = new THREE.Mesh( geometry, material );
 
@@ -41,19 +41,57 @@ var createCube = function() {
 
 }
 
+var createCubix = function() {
+    var geometry2 = new THREE.SphereGeometry( 1, 50, 50 );
+    var material2 = new THREE.MeshBasicMaterial( {color: 0x00ffff} );
+
+    var cube = new THREE.Mesh( geometry2, material2 );
+
+    cube.position.x = 15;
+    cube.position.y = 15;
+    cube.position.z = 0;
+
+    group.add(cube);
+
+}
+
+
 var render = function () {
 
     if(typeof array === 'object' && array.length > 0) {
-        var scale = boost * 0.03;
+        var scale = boost * 0.02;
 		if(boost < 100) {
     		console.log('1')
+
             group.children[1].scale.x = (scale < 1 ? 1 : scale);
+            group.children[1].scale.y = (scale < 1 ? 1 : scale);
+
             group.children[0].scale.x = 1;
-		}else {
+            group.children[0].scale.y = 1;
+            group.children[2].scale.x = 1;
+            group.children[2].scale.y = 1;
+
+		}else if(boost > 100 && boost < 120){
             console.log('2')
+
             group.children[0].scale.x = (scale < 1 ? 1 : scale);
+            group.children[0].scale.y = (scale < 1 ? 1 : scale);
+
             group.children[1].scale.x = 1;
-		}
+            group.children[1].scale.y = 1;
+            group.children[2].scale.x = 1;
+            group.children[2].scale.y = 1;
+		} else {
+            console.log('3')
+
+            group.children[2].scale.x = (scale < 1 ? 1 : scale);
+            group.children[2].scale.y = (scale < 1 ? 1 : scale);
+
+            group.children[0].scale.x = 1;
+            group.children[0].scale.y = 1;
+            group.children[1].scale.x = 1;
+            group.children[1].scale.y = 1;
+        }
 
     }
 
@@ -67,6 +105,7 @@ var render = function () {
 var createAll = function () {
     createSphere();
     createCube();
+    createCubix();
 
     scene.add( group );
 
